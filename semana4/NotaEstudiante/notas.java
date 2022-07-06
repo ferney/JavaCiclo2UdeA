ff12e65cff3fdd5e4da976eac65ea1a3c7fdc3e5
Ferney Chaparro <williamchaparro@unisangil.edu.co>
2022-07-06 12:31:26 -0500

    Add files via upload

---
+++ semana4/NotaEstudiante/notas.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculo_de_notas;
+
+/**
+ *
+ * @author FERNEY CHAPARRO
+ */
+public class notas { 
+
+    //procedemos a crear 2 arrays para 4 estudiantes
+   
+
+   
+    
+   
+    
+    
+public void registrarnota(double n, int i)  {   
+     } 
+
+public void registrarnombre(String nom, int cod) {
+        
+    }
+
+public void calcularpromedio()  {   
+      
+}
+
+public double darnotapromediocurso()  {   
+    
+} 
+
+public String darnotaestudiante(int cod)  { 
+      
+} 
+
+public String darlistadonotas()  {    
+      
+}     
+
+public String darlistaaprobados()  {   
+    
+}   
+
+public String darlistadesaprobados()  {   
+     
+}
+
+   
+
+    
+}     
